2ac320039a8b69eb14deffb8f0ddf1743b21fb9e
Mkisecok <mahmutkisecok@gmail.com>
2022-02-12 07:31:12 +0100

    feat: Create errorhandler and remove error route

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const bodyParser=require('body-parser');
-const { middlewareSecurity }=require('../middlewares/middlewareSecurity');
+const { middlewareSecurity }=require('./middlewares/middlewareSecurity');
 const morgan = require('morgan');
 
 // import routes
-const records = require('./../routes/records');
-const users =require('./../routes/users');
-const orders= require('./../routes/orders');
-const error =require('./../routes/error');
+const records = require('./routes/records');
+const users =require('./routes/users');
+const orders= require('./routes/orders');
+const error =require('./routes/error');
 
 const app=express();
 const port=3000;
@@ -25,7 +25,26 @@ app.use(morgan('tiny'));
 app.use('/records', records);
 app.use('/users', users);
 app.use('/orders', orders);
-app.use('*', error);
+
+app.use('*', (req, res, next ) =>
+{
+    let myError = new Error('This path does not exist');
+    myError.statusCode=404;
+    next(myError);
+});
+
+app.use((error, req, res, next) =>
+{
+    console.log('Created error handler ', error );
+    res.status(error.statusCode);
+    res.send({ 
+        error: 
+            {
+                status:error.statusCode,
+                message:error.message
+            } 
+    });
+});
 
 app.listen(port, () => 
 {
--- src/middlewares/middlewareSecurity.js
+++ src/middlewares/middlewareSecurity.js
@@ -4,7 +4,7 @@ exports.middlewareSecurity=() =>
     {
         res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-        res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        res.header('Access-Control-Allow-Methods', 'GET,POST, OPTIONS, PUT, PATCH, DELETE');
         
         next();
     };
