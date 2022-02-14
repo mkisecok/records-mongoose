241805196268eb2f45b224751615997e735569bc
Mkisecok <mahmutkisecok@gmail.com>
2022-02-14 16:06:52 +0100

    feat: Create html file

--- src/controller/orderController.js
+++ src/controller/orderController.js
@@ -4,23 +4,26 @@ const low=require('lowdb');
 const FileSync=require('lowdb/adapters/FileSync');
 const adapter=new FileSync('db.json');
 const db=low(adapter);
-
+const path = require('path');
 db.defaults({ orders:[] }).write();
 
 const getController= ((req, res) => 
-{
-    const allOrders=db.get('orders');
-    res.status(200).json(allOrders);
+{   
+    // const allOrders=db.get('orders');
+    // res.status(200).json(allOrders);
+    res.status(200).sendFile(path.join(__dirname, '../../public/index.html'));
+
 });
 
 const postController = ((req, res) =>
 {
     let listId=db.get('orders').value().length+1;
     const { productId, quantity } = req.body;
+    console.log(productId, quantity);
 
     db.get('orders').push(
         { listId, productId, quantity }).write();
-    res.status(200).json(`New order ${ productId} posted`);
+    res.status(200).json({ success:true });
 });
 
 const getParamController = ( (req, res) => 
