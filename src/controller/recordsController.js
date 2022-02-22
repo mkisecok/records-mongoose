7834dd8b47cdb1c6f43c74239d46e7008461fb46
Mkisecok <mahmutkisecok@gmail.com>
2022-02-22 14:35:51 +0100

    feat: Implement mongoose and chance

--- src/controller/recordsController.js
+++ src/controller/recordsController.js
@@ -3,56 +3,134 @@
 const basicAuth = require('express-basic-auth');
 const users={ users: { 'mahmut': 'test123' }, unauthorizedResponse: 'Your password or username is false' };
 
-// lowdb
-const low=require('lowdb');
-const FileSync=require('lowdb/adapters/FileSync');
-const adapter=new FileSync('db.json');
-const db=low(adapter);
+// model
+const Record = require('../models/Record');
 
-db.defaults({ records:[] }).write();
+// chance
+const Chance = require('chance');
+const chance = new Chance();
 
 const getController= ((req, res) => 
 {
-    const allRecords=db.get('records');
-    res.status(200).json(allRecords);
+    Record.find({})
+        .then((records) =>
+        {
+            return res.status(200).json({
+                success:true,
+                data: records
+            });
+        })
+        .catch((err) =>
+        {
+            return res.status(400).json({
+                success:false,
+                message:err.message
+            });
+        });
+    
 });
 
 const postController = ( basicAuth(users), (req, res) => 
 {
-    let id=db.get('records').value().length+1;
 
-    const { 
-        title, artist, year, price 
-    }
-    = req.body;
+    newRecord = new Record();
 
-    db.get('records').push( 
-        { 
-            id, title, artist, year, price 
-        }).write();
+    newRecord.title = chance.word();
+    newRecord.artist = chance.name();
+    newRecord.year = parseInt(chance.year({ min: 1980, max: 2022 }));
+    newRecord.price =chance.integer({ min: 5, max: 25 });
 
-    res.status(200).json(`New record ${title} added ! `);
+    newRecord.save((err, record) =>
+    {
+        if(err) throw err;
+
+        res.json(record);
+    });
+    
 });
 
 const getParamController = ( (req, res) => 
 {
     const { id } = req.params;
 
-    res.status(200).send('Get the record with id ' + id);
+    Record.find({ _id:id })
+        .then((record) =>
+        {
+            res.status(200).json({
+                success:true,
+                data:record
+            });
+        })
+        .catch((err) => 
+        {
+            res.status(400).json({
+                success:false,
+                message:err.message
+            });
+        });
+
 });
 
 const putParamController = ( basicAuth(users), (req, res) => 
 {
     const { id } = req.params;
 
-    res.status(200).send('Update the record with id' + id);
+    Record.findOne({ _id:id })
+        .then((record) =>
+        {
+            record.title = req.body.title || record.title;
+            record.artist = req.body.artist || record.artist;
+            record.year = req.body.year || record.year;
+            record.price = req.body.price ||record.price;
+
+            record.save()
+                .then((updatedRecord) => 
+                {
+                    res.status(200).json({
+                        success: true,
+                        newData:req.body,
+                        data:updatedRecord
+                    });
+
+                })
+                .catch((err) => 
+                {
+                    res.status(400).json({
+                        success:false,
+                        message:err.message
+                    });
+                });
+        })
+        .catch((err) => 
+        {
+            res.status(400).json({
+                success:false,
+                message:err.message
+            });
+        });
+
 });
 
 const deleteParamController = (basicAuth(users), (req, res) => 
 {
     const { id } = req.params;
 
-    res.status(200).send('Delete the record with id' + id);
+    Record.deleteOne({ _id: id })
+        .then(() => 
+        {
+            res.status(200).json({
+                success:true,
+                message: `Deleted the data id: ${ id } `
+            });
+        })
+        .catch((err) => 
+        {
+            res.status(400).json({
+                success:false,
+                message: err.message
+            });
+        });
+
 });
 
 module.exports = { 
