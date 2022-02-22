18137e5bb837fc08dfbe6c4239e54373dcb62370
Mkisecok <mahmutkisecok@gmail.com>
2022-02-22 15:41:33 +0100

    feat: Change the db from lowdb into mongoDB with mongoose

--- src/controller/usersController.js
+++ src/controller/usersController.js
@@ -6,42 +6,137 @@ const db=low(adapter);
 
 db.defaults({ users:[] }).write();
 
+// model
+const User =require('../models/User');
+
+// chance
+const Chance = require('chance');
+const chance = new Chance();
+
 const getController = ((req, res) => 
 {
-    const allUsers=db.get('users');
-    res.status(200).json(allUsers);
+
+    User.find({})
+        .then((users) => 
+        {
+            res.status(200).json({
+                success:true,
+                data: users
+            });
+        })
+        .catch((err) => 
+        {
+            res.status(400).json({
+                success:false,
+                message:err.message
+            });
+        });
 });
 const postController = ((req, res) => 
 {
-    let id=db.get('users').value().length+1;
+    
+    newUser = new User();
 
-    const 
-        { 
-            firstName, lastName, email, password 
-        } = req.body;
-    db.get('users').push(
-        { 
-            id, firstName, lastName, email,  password 
-        }).write();
-    res.status(200).json(` ${firstName } ${ lastName } added as new user`);
+    newUser.firstname = chance.first();
+    newUser.lastname = chance.last();
+    newUser.email = chance.email({ domain: 'example.com' });
+    newUser.password = chance.hash({ length: 8 });
+    
+    newUser.save()
+        .then(user => 
+        {
+            res.status(200).json({
+                success:true,
+                data:user
+            });
+        })
+        .catch((err) => 
+        {
+            res.status(400).json({
+                success: false,
+                message: err.message
+            });
+        });
+    
 });
 const getParamController = ((req, res) => 
 {
     const { id } =req.params;
 
-    res.status(200).send('Get a user with id '+ id );
+    User.find({ _id:id })
+        .then(user => 
+        {
+            res.status(200).json({
+                success:true,
+                data:user
+            });
+        })
+        .catch(err => 
+        {
+            res.status(400).json({
+                success:false,
+                message:err.message
+            });
+        });
+
 });
 const putParamController = ((req, res) => 
 {
     const { id } = req.params;
 
-    res.status(200).send('Update a user with id ' + id);
+    User.findOne({ _id: id })
+        .then(user => 
+        {
+            user.firstname = req.body.firstname || user.firstname;
+            user.lastname = req.body.lastname || user.lastname;
+            user.email = req.body.email || user.email;
+            user.password = req.body.password || user.password;
+
+            user.save()
+                .then(updatedUser => 
+                {
+                    res.status(200).json({
+                        success:true,
+                        newData: req.body,
+                        data: updatedUser
+                    });
+                })
+                .catch(err => 
+                {
+                    res.status(400).json({
+                        success:false,
+                        message:err.message
+                    });
+                });
+
+        })
+        .catch(err => 
+        {
+            res.status(400).json({
+                success:false,
+                message:err.message
+            });
+        });
 });
 const deleteParamController =((req, res) => 
 {
     const { id } = req.params;
 
-    res.status(200).send('Remove a user with id ' + id );
+    User.deleteOne({ _id: id })
+        .then(() => 
+        {
+            res.status(200).json({
+                success:true,
+                message: `Deleted the user id: ${ id }`
+            });
+        })
+        .catch(err =>
+        {
+            res.status(400).json({
+                success:false,
+                message:err.message
+            });
+        });
 });
 
 module.exports=
